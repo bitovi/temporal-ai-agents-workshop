adb546e1a08ebcf575c31ec81800ee94eb5e5165
Kevin Phillips <kevin@bitovi.com>
2025-07-08 21:04:28 -0500

    removing unused temporal client code

--- 0-environment-setup/src/main/java/bitovi/common/TemporalClient.java
+++ 0-environment-setup/src/main/java/bitovi/common/TemporalClient.java
@@ -10,34 +10,10 @@ public class TemporalClient {
 		Config config = new Config();
 		String temporalHostAndPort = config.getProperty("TEMPORAL_HOST_PORT");
 		String temporalNamespace = config.getProperty("TEMPORAL_NAMESPACE");
-		// String temopralClientCertPath =
-		// properties.getProperty("TEMPORAL_CLIENT_CERT_PATH");
-		// String temporalClientKeyPath =
-		// properties.getProperty("TEMPORAL_CLIENT_CERT_KEY_PATH");
-		// String temporalCaCertPath = properties.getProperty("TEMPORAL_CA_CERT_PATH");
-		// String apiKey = config.getProperty("TEMPORAL_API_KEY");
-
-		// InputStream clientCert = new FileInputStream(temopralClientCertPath);
-		// InputStream clientKey = new FileInputStream(temporalClientKeyPath);
-		// InputStream caCert = new FileInputStream(temporalCaCertPath);
-
-		// SslContext sslContext = GrpcSslContexts.configure(
-		// SslContextBuilder.forClient()
-		// .keyManager(clientCert, clientKey)
-		// .trustManager(caCert))
-		// .build();
-
-		// SslContext sslContext = SimpleSslContextBuilder
-		// .forPKCS8(clientCert, clientKey)
-		// .forPKCS12(clientKey)
-		// .build();
 
 		WorkflowServiceStubsOptions serviceOptions = WorkflowServiceStubsOptions
 				.newBuilder()
 				.setTarget(temporalHostAndPort)
-				// .setSslContext(sslContext)
-				// .setEnableHttps(true)
-				// .addApiKey(() -> apiKey)
 				.build();
 
 		WorkflowServiceStubs service = WorkflowServiceStubs.newServiceStubs(serviceOptions);
