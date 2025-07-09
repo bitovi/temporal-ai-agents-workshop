a1c3956e0851196f58f18b730124d4733c36acc8
Kevin Phillips <kevin@bitovi.com>
2025-07-08 21:42:33 -0500

    adding scaffold of exercise 3

---
+++ 3-tool-calling/README.md
@@ -0,0 +1,8 @@
+
+# Exercise 3 - Tool Calling
+
+## Goals
+
+## What you need to know
+
+## Solution
---
+++ 3-tool-calling/src/main/java/bitovi/ToolCallingWorkflow.java
@@ -0,0 +1,11 @@
+package bitovi;
+
+import io.temporal.workflow.WorkflowInterface;
+import io.temporal.workflow.WorkflowMethod;
+
+@WorkflowInterface
+public interface ToolCallingWorkflow {
+
+	@WorkflowMethod
+	String execute();
+}
