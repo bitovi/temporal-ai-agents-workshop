21be7375900e7d61c4fc90b857118a01dd921165
Kevin Phillips <kevin@bitovi.com>
2025-07-21 12:10:10 -0500

    adding prompt engineering example

---
+++ 1-prompt-engineering/src/main/java/bitovi/PromptEngineeringClient.java
@@ -0,0 +1,36 @@
+package bitovi;
+
+import bitovi.common.TemporalClient;
+import bitovi.common.Config;
+
+import io.temporal.client.WorkflowClient;
+import io.temporal.client.WorkflowOptions;
+
+public class PromptEngineeringClient {
+
+	public static void main(String[] args) {
+		Config config = new Config();
+		String userId = config.getProperty("USER_ID");
+		String taskQueue = config.getProperty("TEMPORAL_TASK_QUEUE");
+
+		WorkflowClient temporalClient = TemporalClient.getTemporalClient();
+
+		String uuid = java.util.UUID.randomUUID().toString();
+		String workflowId = "prompt-engineering-workflow-" + uuid + "-" + userId;
+
+		WorkflowOptions workflowOptions = WorkflowOptions
+				.newBuilder()
+				.setTaskQueue(taskQueue)
+				.setWorkflowId(workflowId)
+				.build();
+
+		PromptEngineeringWorkflow workflow = temporalClient
+				.newWorkflowStub(PromptEngineeringWorkflow.class, workflowOptions);
+
+		String response = workflow.execute(
+				"7:44 AM: How do I reset my password?",
+				"7:51 AM: To reset your password, go to the login page and click on 'Forgot Password?'. Follow the instructions to reset your password via email or SMS.");
+
+		System.out.println("Workflow Executed: " + response);
+	}
+}
---
+++ 1-prompt-engineering/src/main/java/bitovi/activities/ActivitiesImpl.java
@@ -0,0 +1,79 @@
+package bitovi.activities;
+
+import java.io.InputStream;
+import java.util.IllegalFormatException;
+
+import bitovi.common.Config;
+
+import io.temporal.failure.ApplicationFailure;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.bedrockruntime.BedrockRuntimeClient;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseRequest;
+import software.amazon.awssdk.services.bedrockruntime.model.ConverseResponse;
+import software.amazon.awssdk.services.bedrockruntime.model.Message;
+import software.amazon.awssdk.services.bedrockruntime.model.ContentBlock;
+import software.amazon.awssdk.services.bedrockruntime.model.ConversationRole;
+import software.amazon.awssdk.services.bedrockruntime.model.InferenceConfiguration;
+
+public class ActivitiesImpl implements Activities {
+	@Override
+	public String promptLLM(String userQuestion, String agentResponse) throws ApplicationFailure {
+		Config config = new Config();
+
+		String AWS_ACCESS_KEY_ID = config.getProperty("AWS_ACCESS_KEY_ID");
+		String AWS_SECRET_ACCESS_KEY = config.getProperty("AWS_SECRET_ACCESS_KEY");
+		String AWS_SESSION_TOKEN = config.getProperty("AWS_SESSION_TOKEN");
+		String AWS_REGION = config.getProperty("AWS_REGION");
+
+		String AWS_MODEL_ID = config.getProperty("AWS_MODEL_ID");
+
+		try {
+			BedrockRuntimeClient bedrockRuntimeClient = BedrockRuntimeClient.builder()
+					.credentialsProvider(
+							StaticCredentialsProvider.create(
+									AwsSessionCredentials.create(
+											AWS_ACCESS_KEY_ID,
+											AWS_SECRET_ACCESS_KEY,
+											AWS_SESSION_TOKEN)))
+					.region(Region.of(AWS_REGION))
+					.build();
+
+			InputStream promptStream = getClass().getClassLoader().getResourceAsStream("prompt.txt");
+			String promptTemplate = new String(promptStream.readAllBytes()).trim().replaceAll("%(?!s)", "%%");
+
+			String prompt = String.format(promptTemplate, userQuestion, agentResponse);
+
+			Message policyMessage = Message.builder()
+					.role(ConversationRole.USER)
+					.content(ContentBlock.fromText(prompt))
+					.build();
+
+			InferenceConfiguration inferenceConfig = InferenceConfiguration.builder()
+					.maxTokens(2000)
+					.temperature(1.0f)
+					.build();
+
+			ConverseRequest converseRequest = ConverseRequest.builder()
+					.modelId(AWS_MODEL_ID)
+					.messages(policyMessage)
+					.inferenceConfig(inferenceConfig)
+					.build();
+
+			ConverseResponse response = bedrockRuntimeClient.converse(converseRequest);
+			String completion = response.output().message().content().get(0).text();
+			System.out.println("LLM Response: " + completion);
+
+			return "LLM Response (Claude): " + completion;
+		} catch (IllegalFormatException e) {
+			throw ApplicationFailure.newNonRetryableFailure(
+					"Failed to format prompt: " + e.getMessage(),
+					"PromptFormattingError");
+		} catch (Exception e) {
+			throw ApplicationFailure.newNonRetryableFailure(
+					"Failed to connect to Bedrock: " + e.getMessage(),
+					"BedrockError");
+		}
+	};
+}
