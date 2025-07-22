207e698e72d69f41815a9f0a272a69af9cd7f769
Kevin Phillips <kevin@bitovi.com>
2025-07-21 20:47:43 -0500

    updating Worker print statements

--- 1-prompt-engineering/src/main/java/bitovi/PromptEngineeringWorker.java
+++ 1-prompt-engineering/src/main/java/bitovi/PromptEngineeringWorker.java
@@ -25,7 +25,7 @@ public class PromptEngineeringWorker {
 
 			factory.start();
 
-			System.out.println("Temporal Worker started. Press Ctrl+C to exit.");
+			System.out.println("Exercise 1 Temporal Worker started. Press Ctrl+C to exit.");
 
 			// Keep the worker running
 			Thread.currentThread().join();
