304fd0eb358404f822c744ed6b0f1b3bf763f96f
Kevin Phillips <kevin@bitovi.com>
2025-07-21 21:40:30 -0500

    sharing config.properties between examples

--- 0-environment-setup/src/main/java/bitovi/common/Config.java
+++ 0-environment-setup/src/main/java/bitovi/common/Config.java
@@ -2,16 +2,14 @@ package bitovi.common;
 
 import java.util.Properties;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.FileInputStream;
 
 public class Config {
 	private static Properties properties;
 
 	public Config() {
 		properties = new Properties();
-		try (InputStream input = new FileInputStream("config.properties")) {
-			properties.load(input);
+		try {
+			properties.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
