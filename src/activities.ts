87c9c171b704a7afe05ba2ef25f469b6775d94fc
Kevin Phillips <kevin@bitovi.com>
2025-07-29 11:24:07 -0700

    ollama model calling

--- src/activities.ts
+++ src/activities.ts
@@ -1,3 +1,28 @@
+import dotenv from 'dotenv'
+import { Ollama } from 'ollama'
+
+dotenv.config()
+
 export async function helloActivity(): Promise<string> {
-	return "Hello from the activity!";
+  const ollama = new Ollama({ host: process.env.OLLAMA_HOST_AND_PORT })
+
+  const messages = [
+    { role: 'system', content: 'You are a helpful assistant.' },
+    { role: 'user', content: 'Tell me a short story about a brave knight.' },
+  ]
+
+  const chatResponse = await ollama.chat({
+    model: 'gemma3n:e4b',
+    messages: messages,
+    stream: true,
+  })
+
+	let response = ''
+  for await (const chunk of chatResponse) {
+    if (chunk.message?.content) {
+			process.stdout.write(chunk.message.content)
+			response += chunk.message.content
+    }
+  }
+	return response
 }
--- src/workflows.ts
+++ src/workflows.ts
@@ -4,7 +4,7 @@ import * as activities from './activities';
 const {
 	helloActivity
 } = proxyActivities<typeof activities>({
-  startToCloseTimeout: '1 minute',
+  startToCloseTimeout: '5 minutes',
   retry: {
     backoffCoefficient: 1,
     initialInterval: '3 seconds',
