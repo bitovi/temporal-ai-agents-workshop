09b36399e1f524b43b1c875f0ca1215c8fd5ffa9
Mark Repka <mark.repka@yum.com>
2025-08-05 15:44:43 -0400

    subfolders

---
+++ 3-tool-calling/java/src/main/java/bitovi/common/Config.java
@@ -0,0 +1,23 @@
+package bitovi.common;
+
+import java.util.Properties;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileInputStream;
+
+public class Config {
+	private static Properties properties;
+
+	public Config() {
+		properties = new Properties();
+		try (InputStream input = new FileInputStream(".env")) {
+			properties.load(input);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public String getProperty(String key) {
+		return properties.getProperty(key);
+	}
+}
